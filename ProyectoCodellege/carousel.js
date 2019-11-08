a59fcf814e507806325c32b96f3c14f9a69f68bf
mary1429 <marcardenas57@gmail.com>
2019-11-08 08:33:34 -0600

    animaciones

--- ProyectoCodellege/carousel.js
+++ ProyectoCodellege/carousel.js
@@ -1,3 +1,8 @@
 $('.carousel').carousel({
     pause: "false"
 });
+$(document).ready(function(){
+    $("#imagen2").mouseenter(function imagen(){
+        $(".imagen1").css("opacity", "0");
+    });
+});
