6a94b1606e6dd83af74779fc661c995aa6ab1049
EzequielMaciasRivera <ezequielmr678@gmail.com>
2019-11-12 10:44:01 -0600

    Cree la sección de productos y algunas carpetas adicionales

--- ProyectoCodellege/carousel.js
+++ ProyectoCodellege/carousel.js
@@ -1,3 +1,9 @@
 $('.carousel').carousel({
     pause: "false"
 });
+
+	$( document ).ready(function() {
+		$('#menu_on').click(function(){ // Al hacer click...
+		   $('body').toggleClass('visible_menu'); // Añadimos o eliminamos la clase 'visible_menu' al body
+		 })
+	});
